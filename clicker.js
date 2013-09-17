31835ca9842df5d34b933f3ff1fa9e00ea1ac7da
Kriot <natalia@natalia.(none)>
2013-09-17 10:47:15 +0400

    clicker design and code

--- clicker.js
+++ clicker.js
@@ -1,1 +1,24 @@
+var in = 0;
+var out = 0;
+function In()
+{
+	in++;
+}
 
+function Out()
+{
+    out++;
+}
+function NextStation()
+{
+    json = JSON.stringify({type:"inout",in:in,out:out});
+    $.ajax({
+	url:"/server.php",
+	type:"post",
+	data: {json:json},
+	success: function(ret)
+	{
+	    alert(ret);
+	}
+    }
+}
--- clicker.php
+++ clicker.php
@@ -1,15 +1,24 @@
 <?php
 
 ?>
+<html>
+<head>
 <style>
 @import "clicker.css" all;
 </style>
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+<script src="jquery.js"></script>
 <script src="clicker.js"></script>
+</head>
+<body>
 <div class="clicker_box">
-     <div class="clicker_button_left clicker_button">
+     <div class="clicker_button_left clicker_button" onclick="In()">
      	  Out
      </div>
-     <div class="clicker_button_right clicker_button">
+     <div class="clicker_button_right clicker_button" onclick="Out()">
      	  In
      </div>
+     <div class="send_button" onclick="NextStation()">Next Station</div>
 </div>
+</body>
+</html>
